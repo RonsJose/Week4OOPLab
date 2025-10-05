e0a5d422ef13afb0be62da30fb7048d808b4034e
RonsJose <g00464215@atu.ie>
2025-10-05 21:49:13 +0100

    Added an arrayList to hold all the students information.

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -1,22 +1,36 @@
 package ie.atu;
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class StudentApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Student student1;
 
+        List<Student> studentsList = new ArrayList<Student>();
+
+        int studentNum=0,count=0;
         System.out.println("Please enter the number of students: ");
+        studentNum = sc.nextInt();
+        sc.nextLine();
 
-        System.out.println("Please enter your name: ");
-        String name = sc.nextLine();
-        System.out.println("Please enter your email: ");
-        String email = sc.nextLine();
-        System.out.println("Please enter your studentID: ");
-        String studentID = sc.nextLine();
+        while(count<studentNum)
+        {
+            System.out.println("Please enter your name: ");
+            String name = sc.nextLine();
+            System.out.println("Please enter your email: ");
+            String email = sc.nextLine();
+            System.out.println("Please enter your studentID: ");
+            String studentID = sc.nextLine();
 
-        Student student1= new Student();
-        student1.setName(name);
-        student1.setEmail(email);
-        student1.setStudentID(studentID);
+            student1= new Student();
+            student1.setName(name);
+            student1.setEmail(email);
+            student1.setStudentID(studentID);
+            studentsList.add(student1);
+
+            count++;
+        }
     }
 }
