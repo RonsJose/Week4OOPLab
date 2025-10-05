6b13b3ca9d3e6b906a0f1d8def82e25c3d54aff4
RonsJose <g00464215@atu.ie>
2025-10-05 20:42:02 +0100

    Student Class fields, getters,setters, & constructors added.
    StudentApp Class input method & population made

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -1,4 +1,22 @@
 package ie.atu;
+import java.util.Scanner;
 
 public class StudentApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter the number of students: ");
+
+        System.out.println("Please enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Please enter your email: ");
+        String email = sc.nextLine();
+        System.out.println("Please enter your studentID: ");
+        String studentID = sc.nextLine();
+
+        Student student1= new Student();
+        student1.setName(name);
+        student1.setEmail(email);
+        student1.setStudentID(studentID);
+    }
 }
